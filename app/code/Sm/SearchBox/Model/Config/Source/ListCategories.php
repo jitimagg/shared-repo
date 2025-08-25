1493f1c25fc3d9397e87c9be0d86246a2e238c6f
jitimagg <jitima.gg@gmail.com>
2025-08-25 17:25:48 +0700

    update before send server
    ใส่ลิงก์ทุกอย่างแล้ว อัพขึ้นไปก่อน เหลือหน้าแบรนด์ของสินค้านั้นๆ

--- app/code/Sm/SearchBox/Model/Config/Source/ListCategories.php
+++ app/code/Sm/SearchBox/Model/Config/Source/ListCategories.php
@@ -71,7 +71,8 @@ class ListCategories
                 while (count($stack) > 0) {
                     $opt = array_pop($stack);
                     $option = [
-                        'label' => ($opt->level > 1 ? str_repeat('- - ', $opt->level - 1) : '') . $opt->label,
+                        // 'label' => ($opt->level > 1 ? str_repeat('- - ', $opt->level - 1) : '') . $opt->label,
+                        'label' => $opt->label,
                         'value' => $opt->value
                     ];
                     array_push($options, $option);
